271f5f302f1fec305423668357ae504c7c8a3350
NtskwK <natsukawa247@outlook.com>
2026-02-03 13:34:05 +0800

    feat(router): add vue-router and view components structure

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import CesiumView from './CesiumView.vue';
+import  BottomViews from './BottomViews/Index.vue';
+import  ControllerViews from './ControllerViews/Index.vue';
 </script>
 
 <template>
@@ -8,9 +10,13 @@ import CesiumView from './CesiumView.vue';
       <div class="flex-5 h-full bg-blue-500">
         <CesiumView/>
       </div>
-      <div class="flex-1  bg-yellow-500">form</div>
+      <div class="flex-1  bg-yellow-500">
+        <BottomViews/>
+      </div>
     </div>
-    <div class="flex-1 min-h-0 p-10 bg-red-500">controller</div>
+    <div class="flex-1 min-h-0 p-10 bg-red-500">
+      <ControllerViews/>
+    </div>
   </div>
 </template>
 
