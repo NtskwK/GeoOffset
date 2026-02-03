d07537a6ebf01a69079d5166cd34f24cf4df52dd
NtskwK <natsukawa247@outlook.com>
2026-02-03 13:55:01 +0800

    feat(system): system status getter with cpu cores and detailed ram info

--- src-tauri/src/systemstatus/lib.rs
+++ src-tauri/src/systemstatus/lib.rs
@@ -1,6 +1,27 @@
 #[derive(ts_rs::TS)]
 #[ts(export)]
 struct SystemStatus {
+    cpu_cores: i32,
     cpu_usage: i32,
-    ram_usage: i32,
+    ram_all: usize,
+    ram_usage: usize,
+}
+
+pub fn get_system_status() -> SystemStatus {
+    let mut sys = sysinfo::System::new_all();
+
+    sys.refresh_cpu();
+    thread::sleep(Duration::from_millis(500));
+    sys.refresh_cpu();
+
+    sys.refresh_memory();
+    let total_mem = sys.total_memory();
+    let used_mem = sys.used_memory();
+
+    SystemStatus {
+        cpu_cores: sys.cpus().len() as i32,
+        cpu_usage: sys.global_cpu_info().cpu_usage() as i32,
+        ram_all: total_mem as usize,
+        ram_usage: used_mem as usize,
+    }
 }
