87da928365f558f1671a9114f660e4a067067919
NtskwK <natsukawa247@outlook.com>
2026-02-28 16:52:55 +0800

    feat: add @heroicons/react dependency and update NavBar styling

--- src/views/NavBar.vue
+++ src/views/NavBar.vue
@@ -1,9 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const raster_list = [];
+const vector_list = [];
+const entity_list = [];
+</script>
 
 <template>
-  <nav class="w-50 bg-gray-800">
+  <nav class="w-50 bg-gray-800 pt-10 flex flex-col items-center">
     navbar
-    <ul class="menu bg-base-200 rounded-box lg: mb-64">
+    <ul class="menu bg-base-200 rounded-box lg: mb-64 text-black w-[80%]">
       <li>Home</li>
       <li>
         <details>
